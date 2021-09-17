9467335c99eeb9896905b81f979b8487927db4be
sabaijung <ardsri.s@gmail.com>
2021-09-17 12:11:50 +0700

    เพิ่มไฟล์ สำหรับแสดงข้อมูลคณะ

---
+++ src/Views/Setting/Faculty/ShowFaculty.js
@@ -0,0 +1,56 @@
+import React, { Fragment, useState, useEffect } from "react";
+import { GetAllFaculty } from "../../../services/faculty.service";
+
+export default function ShowFaculty() {
+  const [data, setData] = useState([]);
+
+  useEffect(fetchData);
+
+  async function fetchData() {
+    const res = await GetAllFaculty();
+    if (res.statusCode == "001") {
+      setData(res.data);
+    }
+  }
+
+  return (
+    <Fragment>
+      <div className="row">
+        <div className="col-12 col-md-12 col-lg-12">
+          <div className="card card-primary">
+            <div className="card-header">
+              <h4>ข้อมูลคณะ</h4>
+            </div>
+            <div className="card-body">
+              {/** สำหรับแสดงรายการข้อมูล */}
+              <div className="form-group row">
+                <table className="table table-bordered">
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>รหัสคณะ</th>
+                      <th>ชื่อคณะ</th>
+                      <th></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {data.map((value, index) => (
+                      <tr>
+                        <td>{index + 1}</td>
+                        <td>{value.facultyCode}</td>
+                        <td>{value.facultyName}</td>
+                        <td>
+                          <button className="btn btn-warning">แก้ไข</button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+}
