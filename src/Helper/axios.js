e3f068fc2c5711c34158d5c4da55724b891f171a
sabaijung <ardsri.s@gmail.com>
2021-09-17 12:10:08 +0700

    แก้ไขไฟล์

--- src/Helper/axios.js
+++ src/Helper/axios.js
@@ -6,7 +6,6 @@ export default axios.create({
   headers: {
     "Content-Type": "application/x-www-form-urlencoded",
     Accept: "application/json",
-
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
   },
