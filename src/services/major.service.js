a3117c5ad1148e8f756789c8186f58bfcfc428ed
sabaijung <ardsri.s@gmail.com>
2021-09-17 12:11:27 +0700

    เพิ่มไฟล์ service ของข้อมูลสาขาวิชา

--- src/services/major.service.js
+++ src/services/major.service.js
@@ -6,7 +6,7 @@ import { URLLOCAL } from "../Helper/baseURL";
 export async function GetAllMajor() {
   try {
     const response = await Instance.get(URLLOCAL + "Major/GetMajor");
-    console.log("re:" + response.data);
+    //.log("re:" + response.data);
     return await response.data;
   } catch (error) {
     console.log("error", error);
