e988a5498356af3ddbdd66eaad7f93f385594a80
sabaijung <ardsri.s@gmail.com>
2021-10-10 15:44:19 +0700

    เพิ่ม function สำหรับ call api ในการแก้ไข และลบข้อมูล

--- src/services/faculty.service.js
+++ src/services/faculty.service.js
@@ -1,23 +1,56 @@
 import Instance from "../Helper/axios";
 import { URLLOCAL } from "../Helper/baseURL";
 
-export async function GetAllFaculty() {
+export async function getAllFaculty(pageSize, currentPage, search) {
   try {
-    const response = await Instance.get(URLLOCAL + "Faculty/GetFaculty");
+    const response = await Instance.get(
+      URLLOCAL +
+        "Faculty/GetFaculty?pageSize=" +
+        pageSize +
+        "&currentPage=" +
+        currentPage +
+        "&search=" +
+        search
+    );
     return await response.data;
   } catch (error) {
     console.log("error", error);
   }
 }
 
-export async function SaveFaculty(data) {
+export async function saveFaculty(data) {
   try {
+    // console.log("data1:" + JSON.stringify(data));
     const response = await Instance.post(
       URLLOCAL + "Faculty/SaveFaculty",
       data
     );
     return await response.data;
   } catch (error) {
-    console.log("error", error);
+    //console.log("error", error.message);
+    console.log(error.response.request._response);
+  }
+}
+
+export async function updateFaculty(facultyCode, data) {
+  try {
+    const response = await Instance.put(
+      URLLOCAL + "Faculty/UpdateFaculty?facultyCode=" + facultyCode,
+      data
+    );
+    return await response.data;
+  } catch (error) {
+    console.log("error", error.message);
+  }
+}
+
+export async function deleteFaculty(facultyCode) {
+  try {
+    const response = await Instance.delete(
+      URLLOCAL + "Faculty/DeleteFaculty?facultyCode=" + parseInt(facultyCode)
+    );
+    return await response.data;
+  } catch (error) {
+    console.log("error", error.message);
   }
 }
