1ab0b8185cbc68ac50f393164e4bfb88a5e66804
sabaijung <ardsri.s@gmail.com>
2021-10-10 15:43:04 +0700

    แก้ไข axios

--- src/Helper/axios.js
+++ src/Helper/axios.js
@@ -4,9 +4,18 @@ import { URLLOCAL } from "../Helper/baseURL";
 export default axios.create({
   baseURL: URLLOCAL,
   headers: {
-    "Content-Type": "application/x-www-form-urlencoded",
+    "Content-Type": "application/json",
     Accept: "application/json",
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
   },
 });
+
+/**
+ *  headers: {
+    "Content-Type": "application/x-www-form-urlencoded,application/json",
+    Accept: "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+  },
+ */
