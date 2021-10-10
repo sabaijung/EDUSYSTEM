9b65594e478ac553b24768498b93ead16e891623
sabaijung <ardsri.s@gmail.com>
2021-10-10 15:45:38 +0700

    เพิ่มฟังก์ชันสำหรับค้นหา แก้ไข ลบข้อมูล การแบ่งหน้า

--- src/Views/Setting/Faculty/ShowFaculty.js
+++ src/Views/Setting/Faculty/ShowFaculty.js
@@ -1,44 +1,175 @@
 import React, { Fragment, useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
-import { GetAllFaculty } from "../../../services/faculty.service";
+import Pagination from "@material-ui/lab/Pagination";
+import Swal from "sweetalert2";
+import Icon from "../../../assets/icons/icons";
+import SVGarrowClockwise from "../../../assets/svgs/SVGarrowClockwise";
+
+import {
+  getAllFaculty,
+  deleteFaculty,
+} from "../../../services/faculty.service";
 
 export default function ShowFaculty() {
   const history = useHistory();
   const [data, setData] = useState([]);
+  const [page, setPage] = useState({
+    currentPage: 0,
+    lastPage: 1,
+    totalRow: 0,
+  });
+  const [pageNo, setPageNo] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
+  const [search, setSearch] = useState("");
 
-  useEffect(fetchData);
+  useEffect(fetchData, [pageNo, pageSize, search]);
 
   async function fetchData() {
-    const res = await GetAllFaculty();
+    const res = await getAllFaculty(pageSize, pageNo, search);
     if (res.statusCode == "001") {
-      setData(res.data);
+      let pagination = res.pagin;
+      if (pagination.totalRow > 0) {
+        setData(res.data);
+        setPage({
+          currentPage: pagination.currentPage,
+          lastPage: pagination.totalPage,
+          totalRow: pagination.totalRow,
+        });
+      }
+    } else {
     }
   }
+
+  const searchData = (e) => {
+    e.preventDefault();
+    setData([]);
+    fetchData();
+  };
+
+  const clearData = (e) => {
+    e.preventDefault();
+    setData([]);
+    setSearch("");
+    setPageNo(1);
+    setPageSize(10);
+    fetchData();
+  };
+
+  const deleteData = async function (e, prmFacultyCode, prmFacultyName) {
+    e.preventDefault();
+    const sweetConfirm = await new Swal({
+      className: "bg-modal-red",
+      icon: "question",
+      iconColor: "red",
+      //dangerMode: true,
+      text: "คุณต้องการลบข้อมูลคณะ" + prmFacultyName + "ใช่หรือไม่?",
+      showCancelButton: true,
+      confirmButtonText: "ใช่",
+      cancelButtonText: "ยกเลิก",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        return true;
+      }
+    });
+
+    if (sweetConfirm) {
+      const result = await deleteFaculty(prmFacultyCode);
+      if (result.statusCode === "001") {
+        new Swal({
+          title: "สำเร็จ!",
+          text: "",
+          icon: "success",
+          showConfirmButton: false,
+          button: "ปิด",
+          timer: 1500,
+        });
+        fetchData();
+      }
+    }
+  };
+
+  const handleChangePage = (e, newPage) => {
+    e.preventDefault();
+    setPageNo(newPage);
+  };
 
   return (
     <Fragment>
       <div className="row">
         <div className="col-12 col-md-12 col-lg-12">
-          <div className="card card-primary">
+          <div className="card card-primary" style={{ minHeight: "550px" }}>
             <div className="card-header">
               <h4>ข้อมูลคณะ</h4>
             </div>
             <div className="card-body">
-              <div>
-                <button
-                  class="btn btn-primary"
-                  onClick={(e) => {
-                    history.push("/FFaculty");
-                  }}
-                >
-                  เพิ่มข้อมูล
-                </button>
+              <div className="row">
+                <div className="row col-md-12">
+                  <div className="col-md-4">
+                    <div className="form-group">
+                      <div className="input-group input-group-sm">
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="ค้นหาชื่อคณะ"
+                          aria-label=""
+                          value={search}
+                          onChange={(e) => {
+                            setSearch(e.target.value);
+                          }}
+                        />
+                        <div className="input-group-append">
+                          <button
+                            className="btn btn-primary"
+                            type="button"
+                            onClick={(e) => {
+                              searchData(e);
+                            }}
+                          >
+                            <Icon
+                              icon="search"
+                              viewBox="0 0 16 16"
+                              color="#FDFEFE"
+                              size={16}
+                              className=""
+                            />
+                          </button>
+                          <button
+                            type="reset"
+                            className="btn btn-secondary"
+                            onClick={(e) => {
+                              clearData(e);
+                            }}
+                          >
+                            <SVGarrowClockwise color="#FDFEFE" />
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-8 text-right">
+                    <button
+                      className="btn btn-primary"
+                      onClick={(e) => {
+                        history.push("/FFaculty");
+                      }}
+                    >
+                      <Icon
+                        icon="add"
+                        viewBox="0 0 16 16"
+                        color="#FDFEFE"
+                        size={16}
+                        className=""
+                      />{" "}
+                      เพิ่มใหม่
+                    </button>
+                  </div>
+                </div>
               </div>
               {/** สำหรับแสดงรายการข้อมูล */}
-              <div className="form-group row">
-                <table className="table table-bordered">
+              <div className="row" style={{ marginTop: "10px" }}>
+                <table className="table table-bordered table-sm">
                   <thead>
-                    <tr>
+                    <tr className="text-center">
                       <th>#</th>
                       <th>รหัสคณะ</th>
                       <th>ชื่อคณะ</th>
@@ -47,18 +178,67 @@ export default function ShowFaculty() {
                   </thead>
                   <tbody>
                     {data.map((value, index) => (
-                      <tr>
+                      <tr key={value.facultyCode}>
                         <td>{index + 1}</td>
                         <td>{value.facultyCode}</td>
                         <td>{value.facultyName}</td>
-                        <td>
-                          <button className="btn btn-warning">แก้ไข</button>
+                        <td className="text-center">
+                          <button
+                            className="btn btn-warning btn-sm"
+                            onClick={(e) => {
+                              history.push("/fFaculty", { value: value });
+                            }}
+                          >
+                            <Icon
+                              icon="pencil"
+                              viewBox="0 0 16 16"
+                              color="#FDFEFE"
+                              size={16}
+                              className=""
+                            />
+                          </button>
+                          &nbsp;
+                          <button
+                            className="btn btn-danger btn-sm"
+                            onClick={(e) => {
+                              deleteData(
+                                e,
+                                value.facultyCode,
+                                value.facultyName
+                              );
+                              return false;
+                            }}
+                          >
+                            <Icon
+                              icon="trash"
+                              viewBox="0 0 16 16"
+                              color="#FDFEFE"
+                              size={16}
+                              className=""
+                            />
+                          </button>
                         </td>
                       </tr>
                     ))}
                   </tbody>
                 </table>
               </div>
+              {/** แสดง pagination */}
+              <div className="d-flex justify-content-between">
+                <div>
+                  ทั้งหมด <strong>{page.totalRow}</strong> รายการ
+                </div>
+                <Pagination
+                  count={parseInt(page.lastPage)}
+                  page={pageNo}
+                  color="primary"
+                  size="small"
+                  defaultPage={6}
+                  siblingCount={1}
+                  onChange={handleChangePage}
+                />
+              </div>
+              {/** ปิด div ของ paging */}
             </div>
           </div>
         </div>
