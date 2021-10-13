caad2953c2a728cde4a1cf77f65859558ffe1d5c
sabaijung <ardsri.s@gmail.com>
2021-10-13 16:31:46 +0700

    เพิ่มไฟล์ major service

--- src/services/major.service.js
+++ src/services/major.service.js
@@ -3,6 +3,16 @@ import Instance from "../Helper/axios";
 
 import { URLLOCAL } from "../Helper/baseURL";
 
+export async function getAllFaculty() {
+  try {
+    const response = await Instance.get(URLLOCAL + "Faculty/GetAllFaculty");
+    // console.log("res:" + JSON.stringify(response));
+    return await response.data;
+  } catch (error) {
+    console.log("error", error);
+  }
+}
+
 export async function GetAllMajor() {
   try {
     const response = await Instance.get(URLLOCAL + "Major/GetMajor");
