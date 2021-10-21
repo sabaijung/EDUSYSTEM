cda4822902f6c146a04e90e50fed2fa517bebffa
sabaijung <ardsri.s@gmail.com>
2021-10-21 14:52:15 +0700

    เพิ่มการ validate ข้อมูล

--- src/Views/Profile/Profile.js
+++ src/Views/Profile/Profile.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { Formik, Form, ErrorMessage } from "formik";
+import Input from "../../components/Input";
 import DropdownList from "../../components/DropdownList";
+import Swal from "sweetalert2";
 import { monthTH } from "../../Data/month-th.json";
+import { saveStudent } from "../../services/student.service";
+import validateStudent from "../Setting/Student/ValidateStudent";
 
 export default class Profile extends React.Component {
   constructor() {
@@ -9,6 +13,23 @@ export default class Profile extends React.Component {
     this.state = {
       initial: [],
       year: "",
+      stdID: "0",
+      titleID: "",
+      name: "",
+      lastName: "",
+      day: "",
+      month: "",
+      year: "",
+      phone: "",
+      email: "",
+      address: "",
+      districtID: "",
+      amphur: "",
+      province: "",
+      postcode: "",
+      username: "",
+      password: "",
+      confirmPassword: "",
     };
   }
 
@@ -37,10 +58,37 @@ export default class Profile extends React.Component {
     this.fetchInitial();
   }
 
+  async action(data) {
+    let res = "";
+    data.birthday = `${data.year + "-" + data.month + "-" + data.day}`;
+    if (data.stdID === "0") {
+      res = await saveStudent(data);
+    }
+
+    if (res !== undefined) {
+      if (res.statusCode === "003") {
+        Swal.fire({
+          icon: "success",
+          title: "บันทึกข้อมูลสำเร็จ",
+          showConfirmButton: false,
+          timer: 500,
+        });
+      } else {
+        Swal.fire({
+          icon: "warning",
+          title: "บันทึกข้อมูลไม่สำเร็จ",
+          showConfirmButton: false,
+          timer: 500,
+        });
+      }
+    }
+  }
+
+  //componentWillUnmount() {}
+
   render() {
     var year = new Date().getFullYear();
     let years = Array.from(new Array(3), (e, index) => year - index);
-
     return (
       <div>
         <div className="col-12 col-md-12 col-lg-12">
@@ -50,26 +98,29 @@ export default class Profile extends React.Component {
             </div>
             <div className="card-body">
               <Formik
-                // validationSchema={Schema}
-                validate={(values) => {
-                  const errors = {};
-                  if (!values.majorName) {
-                    errors.majorName = "จำเป็นต้องระบุข้อมูล";
-                  }
-
-                  if (!values.facultyCode) {
-                    errors.facultyCode = "จำเป็นต้องระบุข้อมูล";
-                  }
-
-                  if (!values.isUsed) {
-                    errors.isUsed = "จำเป็นต้องระบุข้อมูล";
-                  }
-                  return errors;
+                validationSchema={validateStudent}
+                initialValues={{
+                  year: "",
+                  stdID: "0",
+                  titleID: "",
+                  name: "",
+                  lastName: "",
+                  day: "",
+                  month: "",
+                  year: "",
+                  phone: "",
+                  email: "",
+                  address: "",
+                  districtID: "",
+                  amphur: "",
+                  province: "",
+                  postcode: "",
+                  username: "",
+                  password: "",
+                  confirmPassword: "",
                 }}
-                initialValues={{}}
                 enableReinitialize={true}
                 onSubmit={(values, { resetForm }) => {
-                  // console.log("values:" + JSON.stringify(values));
                   this.action(values);
                   resetForm();
                 }}
@@ -85,20 +136,19 @@ export default class Profile extends React.Component {
                   handleSubmit,
                   isSubmitting,
                 }) => (
-                  <form onSubmit={handleSubmit}>
+                  <Form onSubmit={handleSubmit}>
                     <div className="form-group row">
-                      <input
+                      <Input
                         type="hidden"
                         name="stdID"
                         className="form-control"
                         onChange={handleChange}
-                        onBlur={handleBlur}
                         value={values.stdID}
                       />
                       <div className="col-md-3">
                         <label>คำนำหน้าชื่อ</label>
                         <DropdownList
-                          name="initialID"
+                          name="titleID"
                           defaultInputValue={this.state.initialName}
                           getOptionLabel={(option) => option.initialName}
                           getOptionValue={(option) => option.initialID}
@@ -106,31 +156,35 @@ export default class Profile extends React.Component {
                           placeholder={"--กรุณาเลือก--"}
                           value={values.initial}
                           onChange={async (v) => {
-                            setFieldValue("initialID", v.initialID);
+                            setFieldValue("titleID", v.initialID);
                           }}
                           onBlur={handleBlur}
+                          errors={errors}
+                          touched={touched}
                         />
                       </div>
                       <div className="col-md-5">
                         <label>ชื่อ</label>
-                        <input
+                        <Input
                           type="text"
                           name="name"
                           className="form-control"
                           onChange={handleChange}
-                          onBlur={handleBlur}
                           value={values.name}
+                          errors={errors}
+                          touched={touched}
                         />
                       </div>
                       <div className="col-md-4">
                         <label>นามสกุล</label>
-                        <input
+                        <Input
                           type="text"
                           name="lastName"
                           className="form-control"
                           onChange={handleChange}
-                          onBlur={handleBlur}
                           value={values.lastName}
+                          errors={errors}
+                          touched={touched}
                         />
                       </div>
                     </div>
@@ -139,17 +193,19 @@ export default class Profile extends React.Component {
                       <div className="col-md-2">
                         <label>วันเดือนปีเกิด</label>
                         <select
-                          name="day"
+                          name="year"
                           className="form-control"
-                          value={values.day}
+                          value={values.year}
                           onChange={(e) => {
-                            setFieldValue("day", e.target.value);
+                            setFieldValue("year", e.target.value);
                           }}
+                          errors={errors}
+                          touched={touched}
                         >
-                          <option value=""> -- วัน -- </option>
-                          {Array.from(Array(31), (e, i) => (
-                            <option value={i + 1 < 10 ? "0" + (i + 1) : i + 1}>
-                              {i + 1}
+                          <option value=""> -- ปี -- </option>
+                          {years.map((year, index) => (
+                            <option key={`year${index}`} value={year}>
+                              {year + 543}
                             </option>
                           ))}
                         </select>
@@ -163,10 +219,12 @@ export default class Profile extends React.Component {
                           onChange={(e) => {
                             setFieldValue("month", e.target.value);
                           }}
+                          errors={errors}
+                          touched={touched}
                         >
                           <option value=""> -- เดือน -- </option>
                           {monthTH.map((item, index) => (
-                            <option value={item.id} key={item.id}>
+                            <option key={item.id} value={item.id} key={item.id}>
                               {item.monthName}
                             </option>
                           ))}
@@ -174,29 +232,50 @@ export default class Profile extends React.Component {
                       </div>
                       <div className="col-md-2">
                         <select
-                          name="year"
+                          name="day"
                           className="form-control"
                           style={{ marginTop: "30px" }}
-                          value={values.year}
+                          value={values.day}
                           onChange={(e) => {
-                            setFieldValue("year", e.target.value);
+                            setFieldValue("day", e.target.value);
                           }}
+                          errors={errors}
+                          touched={touched}
                         >
-                          <option value=""> -- ปี -- </option>
-                          {years.map((year, index) => (
-                            <option key={`year${index}`} value={year}>
-                              {year + 543}
+                          <option value=""> -- วัน -- </option>
+                          {Array.from(Array(31), (e, i) => (
+                            <option
+                              key={i + 1 < 10 ? "0" + (i + 1) : i + 1}
+                              value={i + 1 < 10 ? "0" + (i + 1) : i + 1}
+                            >
+                              {i + 1}
                             </option>
                           ))}
                         </select>
                       </div>
                       <div className="col-md-3">
                         <label>เบอร์โทรศัพท์</label>
-                        <input type="text" className="form-control" />
+                        <Input
+                          type="text"
+                          name="phone"
+                          className="form-control"
+                          onChange={handleChange}
+                          value={values.phone}
+                          errors={errors}
+                          touched={touched}
+                        />
                       </div>
                       <div className="col-md-3">
                         <label>อีเมล</label>
-                        <input type="text" className="form-control" />
+                        <Input
+                          type="text"
+                          name="email"
+                          className="form-control"
+                          onChange={handleChange}
+                          value={values.email}
+                          errors={errors}
+                          touched={touched}
+                        />
                       </div>
                     </div>
 
@@ -213,23 +292,63 @@ export default class Profile extends React.Component {
                     <div className="form-group row">
                       <div className="col-md-3">
                         <label>บ้านเลขที่</label>
-                        <input type="text" className="form-control" />
+                        <Input
+                          type="text"
+                          name="address"
+                          className="form-control"
+                          onChange={handleChange}
+                          value={values.address}
+                          errors={errors}
+                          touched={touched}
+                        />
                       </div>
                       <div className="col-md-3">
                         <label>ตำบล</label>
-                        <input type="text" className="form-control" />
+                        <Input
+                          type="text"
+                          name="districtID"
+                          className="form-control"
+                          onChange={handleChange}
+                          value={values.districtID}
+                          errors={errors}
+                          touched={touched}
+                        />
                       </div>
                       <div className="col-md-2">
                         <label>อำเภอ</label>
-                        <input type="text" className="form-control" />
+                        <Input
+                          type="text"
+                          name="amphur"
+                          className="form-control"
+                          onChange={handleChange}
+                          value={values.amphur}
+                          errors={errors}
+                          touched={touched}
+                        />
                       </div>
                       <div className="col-md-2">
                         <label>จังหวัด</label>
-                        <input type="text" className="form-control" />
+                        <Input
+                          type="text"
+                          name="province"
+                          className="form-control"
+                          onChange={handleChange}
+                          value={values.province}
+                          errors={errors}
+                          touched={touched}
+                        />
                       </div>
                       <div className="col-md-2">
                         <label>รหัสไปรษณีย์</label>
-                        <input type="text" className="form-control" />
+                        <Input
+                          type="text"
+                          name="postcode"
+                          className="form-control"
+                          onChange={handleChange}
+                          value={values.postcode}
+                          errors={errors}
+                          touched={touched}
+                        />
                       </div>
                     </div>
 
@@ -238,36 +357,58 @@ export default class Profile extends React.Component {
                     <div className="form-group row">
                       <div className="col-md-4">
                         <label>ชื่อผู้ใช้งาน</label>
-                        <input type="text" className="form-control" />
+                        <Input
+                          type="text"
+                          name="username"
+                          className="form-control"
+                          onChange={handleChange}
+                          value={values.username}
+                          errors={errors}
+                          touched={touched}
+                        />
                       </div>
                       <div className="col-md-4">
                         <label>รหัสผ่าน</label>
-                        <input
+                        <Input
                           type="password"
+                          name="password"
                           className="form-control"
-                          id="password"
+                          onChange={handleChange}
+                          value={values.password}
+                          errors={errors}
+                          touched={touched}
                         />
                       </div>
                       <div className="col-md-4">
                         <label>ยืนยันรหัสผ่าน</label>
-                        <input
+                        <Input
                           type="password"
+                          name="confirmPassword"
                           className="form-control"
-                          id="password"
+                          onChange={handleChange}
+                          value={values.confirmPassword}
+                          errors={errors}
+                          touched={touched}
                         />
                       </div>
                     </div>
 
                     <div className="form-group row">
                       <div className="col-sm-12 text-center">
-                        <button type="submit" class="btn btn-primary">
+                        <button
+                          type="submit"
+                          className="btn btn-primary"
+                          disabled={isSubmitting}
+                        >
                           บันทึก
                         </button>{" "}
                         &nbsp; &nbsp;
-                        <button class="btn btn-secondary">ล้างข้อมูล</button>
+                        <button type="reset" className="btn btn-secondary">
+                          ล้างข้อมูล
+                        </button>
                       </div>
                     </div>
-                  </form>
+                  </Form>
                 )}
               </Formik>
             </div>
