e7ab4f0ccd809345cb48a977e353f8ce757fbbf3
sabaijung <ardsri.s@gmail.com>
2021-10-21 14:51:46 +0700

    เพิ่มไฟล์ service

--- src/services/student.service.js
+++ src/services/student.service.js
@@ -15,3 +15,14 @@ export async function GetAllStudent(pageSize, currentPage) {
     console.log("error", error);
   }
 }
+
+export async function saveStudent(data) {
+  try {
+    console.log("data:" + JSON.stringify(data));
+    const response = await Instance.post("Students/SaveStudent", data);
+    return await response.data;
+  } catch (error) {
+    //console.log("error", error.message);
+    console.log(error.response.request._response);
+  }
+}
